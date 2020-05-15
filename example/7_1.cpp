5ab855bc3604f2a8007314c36c389bbd00abb3ff
Chunxu Zou <471500439@qq.com>
2020-05-15 11:16:09 +0800

    图

---
+++ example/7_1.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 邹春旭 on 2020-05-15.
+//
+
+#include <iostream>
+#include "graph/Graph.h"
+
+using namespace std;
+
+int main() {
+    return 0;
+}
---
+++ graph/Graph.h
@@ -0,0 +1,14 @@
+//
+// Created by 邹春旭 on 2020-05-15.
+//
+
+#ifndef GEEKAL_GRAPH_H
+#define GEEKAL_GRAPH_H
+
+
+class Graph {
+
+};
+
+
+#endif //GEEKAL_GRAPH_H
